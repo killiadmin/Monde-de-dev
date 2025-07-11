0ec499c0487bc050230842df1bcbe31b83968ffd
FILÂTRE Killian <k.filatre@gmail.com>
2025-07-11 14:43:29 +0200

    add nev file pour connect to the database

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -1,13 +1,18 @@
 package com.openclassrooms.mddapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MddApiApplication {
 
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(MddApiApplication.class, args);
-	}
+    }
 
 }
